b05885115bd4f2a0f526b04564d94cd2770cc345
accolade99 <131158528+accolade99@users.noreply.github.com>
2025-01-04 18:28:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("faq1").addEventListener("click", () => {
+    alert("OUR SCHOOL OPERATES FROM 8:00AM TO 3:00PM, MONDAYS TO FRIDAYS");
+});
+
+document.getElementById("faq2").addEventListener("click", () => {
+    alert("KINDLY CLICK ON THE ENROLLMENT BUTTON OR VISIT OUR ADMISSIONS PAGE TO DOWNLOAD AND FILL OUT THE ENROLLMENT FORM. FINALLY, CALL US ON +2348033234149 TO CONCLUDE THE ADMISSIONS PROCESS.");
+});
+
+document.getElementById("faq3").addEventListener("click", () => {
+    alert("We use a variety of methods to keep our parents informed, including regular whatsapp platform updates, parent-teacher conferences and our school's website.");
+});
+document.getElementById("faq4").addEventListener("click", () => {
+    alert("We are committed to providing an inclusive and supportive environment for all pupils. Our special education team works closely with teachers and parents to develop personalized plans that cater for each pupil's need. ");
+});
+document.getElementById("faq5").addEventListener("click", () => {
+    alert("Yes! we'd be delighted to give you a tour of our school. Please contact us on +2348033234149 to schedule a visit.");
+});
+document.getElementById("faq6").addEventListener("click", () => {
+    alert("We value your feedback. If you have a concern or suggestion, please don't hesitate to contact us through our website's contact form or by speaking with a member of our administrative team on +2348033234149.");
+});
