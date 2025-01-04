5caae6ff0a29e19004f9e36cde1f6ebc63139e69
accolade99 <131158528+accolade99@users.noreply.github.com>
2025-01-04 21:08:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("faq1").addEventListener("click", () => {
+    alert("OUR SCHOOL OPERATES FROM 8:00AM TO 3:00PM, MONDAYS TO FRIDAYS");
+});
+
+document.getElementById("faq2").addEventListener("click", () => {
+    alert("PASSIONATE COMMITMENT TO EFFECTIVE AND MEASURABLE DELIVERY OF PRIMARY EDUCATION TO THE VARIOUS STRATA OF THE SOCIETY AND MAKING EDUCATIONAL DELIVERY AVAILABLE AT AFFORDABLE COST.");
+});
+
+document.getElementById("faq3").addEventListener("click", () => {
+    alert("Average class sizes are: 7 - 12 pupils for Reception classes; 10 - 15 pupils for Nursery and Primary classes. ");
+});
+document.getElementById("faq4").addEventListener("click", () => {
+    alert("KINDLY CLICK ON THE ENROLLMENT BUTTON OR VISIT OUR ADMISSIONS PAGE TO DOWNLOAD AND FILL OUT THE ENROLLMENT FORM. FINALLY, CALL US ON +2348033234149 TO CONCLUDE THE ADMISSIONS PROCESS.");
+});
+document.getElementById("faq5").addEventListener("click", () => {
+    alert("Our fees are UNBELIEVABLY AFFORDABLE! We allow payment by instalment and we offer discount/rebate to every 3rd child enrolled from a family. For further enquiry, kindly contact the school office on +2348033234149.");
+});
+document.getElementById("faq6").addEventListener("click", () => {
+    alert("We use a variety of methods to keep our parents informed, including regular whatsapp platform updates, parent-teacher conferences and our school's website.");
+});
+document.getElementById("faq7").addEventListener("click", () => {
+    alert("WE ARE COMMITTED TO PROVIDING AN INCLUSIVE AND SUPPORTIVE ENVIRONMENT FOR ALL PUPILS. OUR SPECIAL EDUCATION TEAM WORKS CLOSELY WITH PARENTS TO ENSURE THAT EVERY CHILD RECEIVES THE SUPPORT THEY NEED TO THRIVE.");
+});
+document.getElementById("faq8").addEventListener("click", () => {
+    alert("Yes! we'd be delighted to give you a tour of our school. Please contact us on +2348033234149 to schedule a visit.");
+});
+document.getElementById("faq9").addEventListener("click", () => {
+    alert("We value your feedback. If you have a concern or suggestion, please don't hesitate to contact us through our website's contact form or by speaking with a member of our administrative team on +2348033234149.");
+});
